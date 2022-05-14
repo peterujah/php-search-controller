4cabb137c7c2f8552171198f5e7183aa179bf887
Chigozie Peter <ujahchigozie@gmail.com>
2022-05-14 17:29:04 +0800

    Update SearchController.php

--- src/SearchController.php
+++ src/SearchController.php
@@ -11,8 +11,8 @@ namespace Peterujah\NanoBlock;
  */
 class SearchController{
 	/**
-     * @var string SQL patterns 
-    */
+	* @var string SQL patterns 
+	*/
 	public const START_WITH_QUERY = "query%";
 	public const END_WITH_QUERY = "%query";
 	public const HAVE_ANY_QUERY = "%query%";
@@ -22,59 +22,62 @@ class SearchController{
 	public const START_END_WITH_QUERY = "query%query";
 
 	/**
-     * @var string SearchController algorithms 
-    */
+	* @var string SearchController algorithms 
+	*/
 	public const OR = "OR";
 	public const AND = "AND";
 	public const NAND = "NAND";
 	public const NOR = "NOR";
 
 	/**
-     * @var string SQL search keywords 
-    */
+	* @var string SQL search keywords 
+	*/
 	public const LIKE = "LIKE";
 	public const NOT_LIKE = "NOT LIKE";
 
 	/**
-     * @var string SQL Query 
-    */
+	* @var string SQL Query 
+	*/
 	private $QueryCondition = "";
 
 	/**
-     * @var string Search query algorithm that needs to be used
-    */
+	* @var string Search query algorithm that needs to be used
+	*/
 	private $searchAlgorithm;
 
 	/**
-     * @var string Search request query value
-    */
+	* @var string Search request query value
+	*/
 	private $searchQuery = null;
 
 	/**
-     * @var array MYSQL database table rows to search from
-    */
+	* @var array MYSQL database table rows to search from
+	*/
 	private $paramArray = array();
 
 	/**
-     * @var string MYSQL database table row for tag value
-    */
+	* @var string MYSQL database table row for tag value
+	*/
 	private $paramTags;
 
 	/**
-     * @var string SQL LIKE query operator to be use
-    */
+	* @var string SQL LIKE query operator to be use
+	*/
 	private $operators;
 
 	/**
-     * @var string SQL query prefix
-    */
+	* @var string SQL query prefix
+	*/
 	private $queryStart;
 
 	/**
-     * @var string SQL query suffix
-    */
+	* @var string SQL query suffix
+	*/
 	private $queryEnd;
 
+	/**
+	* Constructor
+	*/
 	public function __construct($algorithm = self::OR) {
 		$this->searchAlgorithm = $algorithm;
 		$this->operators = self::END_WITH_QUERY;
@@ -83,73 +86,67 @@ class SearchController{
 	}
 		
 	/**
-     * Set database search table  columns.
-     *
-     * @param array          $param columns
-     */
+	* Set database search table  columns.
+	*
+	* @param array          $param columns
+	*/
 	public function setParameter($param=array()){
 		$this->paramArray = $param;
 	}
 
 	/**
-     * Set initial SQL queries.
-     *
-     * @param string          $query query
-     */
+	* Set initial SQL queries.
+	* @param string  $query query
+	*/
 	public function setSQLQuery($query){
 		$this->QueryCondition = $query;
 	}
 
 	/**
-     * Set database search operator pattern.
-     *
-     * @param string          $pattern name
-     */
-	public function setOperators($pattern){
-		$this->operators = $pattern;
+	* Set database search operator pattern.
+	* @param string  $operator name
+	*/
+	public function setOperators($operator){
+		$this->operators = $operator;
 	}
 
 	/**
-     * Set database tag table column name.
-     *
-     * @param string          $column name
-     */
+	* Set database tag table column name.
+	* @param string  $column name
+	*/
 	public function setTags($column){
 		$this->paramTags = $column;
 	}
 
 	/**
-     * Set search query value.
-     *
-     * @param string          $query query value
-     * @return object|SearchController 
-     */
+	* Set search query value.
+	* @param string   $query query value
+	* @return object|SearchController 
+	*/
 	public function setQuery($query){
 		$this->searchQuery = htmlspecialchars($query, ENT_QUOTES, "UTF-8");
 		return $this;
 	}
 	
 	/**
-     * Set query prefix string.
-     *
-     * @param string          $str query prefix
-     */
+	* Set query prefix string.
+	* @param string  $str query prefix
+	*/
 	public function setStart($str){
 		$this->queryStart = $str;
 	}
 
 	/**
-     * Set query suffix string.
-     *
-     * @param string          $str query suffix
-     */
+	* Set query suffix string.
+	* @param string $str query suffix
+	*/
 	public function setEnd($str){
 		$this->queryEnd = $str;
 	}
 
 	/**
-     * Split search query value by space.
-     */
+	* Split search query value by space.
+	*/
 	public function split(){
 		if(strpos($this->searchQuery, " ") !== false) {
 			$this->searchQuery = explode(" ", $this->searchQuery);
@@ -159,11 +156,10 @@ class SearchController{
 	}
 
 	/**
-     * Create SQL query from the specified pattern.
-     *
-     * @param string          $value query value
-     * @return string query
-     */
+	* Create SQL query from the specified pattern.
+	* @param string  $value query value
+	* @return string query
+	*/
 
 	private function format($value) {
 		$queryString = "";
@@ -174,19 +170,26 @@ class SearchController{
 		return $queryString;
 	}
 
+	/**
+	* Builds SQL search query.
+	* @return string query
+	*/
 	private function buildQuery(){
 		return rtrim($this->format($this->searchQuery) , " {$this->queryEnd} ");
 	}
 
+	/**
+	* Builds SQL search query array.
+	* @return array queries 
+	*/
 	private function buildArrayQuery($i = 0){
 		return rtrim($this->format($this->searchQuery[$i]) , " {$this->queryEnd} ");;
 	}
 
 	/**
-     * Determine which search method to use while creating query.
-     *
-     * @return string SQL query
-     */
+	* Determine which search method to use while creating query.
+	* @return string SQL query
+	*/
 	private function buildSQL(){
 		$sql = "";
 		if(!empty($this->paramTags)){
@@ -215,10 +218,9 @@ class SearchController{
 	}
 
 	/**
-     * Execute search query.
-     *
-     * @return string SQL query
-     */
+	* Execute search query.
+	* @return string SQL query
+	*/
 	public function getQuery(){
 		if (!empty($this->searchQuery)){ 
 			if (!empty($this->searchQuery)){ 
@@ -228,17 +230,17 @@ class SearchController{
 						$this->setStart(self::LIKE);
 						$this->setEnd(self::OR);
 					break; 
-				
+
 					case self::AND: 
 						$this->setStart(self::LIKE);
 						$this->setEnd(self::AND);
 					break; 
-				
+
 					case self::NAND: 
 						$this->setStart(self::NOT_LIKE);
 						$this->setEnd(self::AND);
 					break; 
-				
+
 					case self::NOR: 
 						$this->setStart(self::NOT_LIKE);
 						$this->setEnd(self::OR);
